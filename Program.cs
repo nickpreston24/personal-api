021156314735b3d57ba0ca28abd6bc6f3ce99b4f
mikepreston17 <michael.n.preston@gmail.com>
2024-07-01 21:14:30 -0500

    ++ wordpress downloading v1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+using CodeMechanic.Diagnostics;
+using CodeMechanic.FileSystem;
+using CodeMechanic.Scraper;
+using CodeMechanic.Scriptures;
 
+var builder = WebApplication.CreateBuilder(args);
+DotEnv.Load();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,30 +20,85 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+var html_scraper = new HtmlScraperService();
+var teachings_svc = new WordPressReaderService();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// string url = "https://ammoseek.com/ammo/224-valkyrie";
+// 
 
-app.MapGet("/weatherforecast", () =>
+string url = "https://books.toscrape.com/";
+
+
+app.MapGet("/tpot/download", async (int per_page, int page) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    if (per_page <= 0) throw new ArgumentOutOfRangeException(nameof(per_page));
+    if (page <= 0) throw new ArgumentOutOfRangeException(nameof(page));
+
+    // string cwd = Directory.GetCurrentDirectory();
+    // string folder = "teachings";
+    // string save_dir = Path.Combine(cwd, folder);
+
+    var results = await teachings_svc.GetPaginatedRangeOfTeachings(per_page, page);
+
+    // results.FirstOrDefault().Dump("first record");
+    return results;
+
+    // results.Dump("results");
+});
+
+
+app.MapGet("ammo/scrape/anglesharp", async () =>
+{
+    var results = await html_scraper.ScrapeHtmlFromAnglesharp<AmmoseekRow>(url);
+    results.Dump("results");
+});
+
+app.MapGet("ammo/scrape/hap", async () =>
+{
+    List<AmmoseekRow> records = new(0);
+    var response = await html_scraper
+        .ScrapeHtmlTable<AmmoseekRow>(url);
+    response.Dump("response");
+    return records;
+});
+
+app.MapGet("/ammo/scrape/httpclient", async () =>
+{
+    List<AmmoseekRow> records = new(0);
+    var response = await html_scraper.ScrapeHtmlFromHttpClient<AmmoseekRow>(url);
+    response.Dump("response");
+    return records;
+}).WithOpenApi();
 
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+public class AmmoseekRow
+{
+    public string retailer { get; set; } = string.Empty;
+    public string description { get; set; } = string.Empty;
+    public string brand { get; set; } = string.Empty;
+    public string caliber { get; set; } = string.Empty;
+    public string grains { get; set; } = string.Empty;
+    public string limits { get; set; } = string.Empty;
+    public string casing { get; set; } = string.Empty;
+    public string is_new { get; set; } = string.Empty;
+    public string price { get; set; } = string.Empty;
+    public string rounds { get; set; } = string.Empty;
+    public string price_per_round { get; set; } = string.Empty;
+    public string shipping_rating { get; set; } = string.Empty;
+    public string last_update { get; set; } = string.Empty; // last time Ammoseek updated this row.
+
+    // Admin properties
+    public string environment { get; set; } = ""; // Dev or prod
+    public DateTimeOffset created_at { get; set; } = DateTimeOffset.Now;
+    public DateTimeOffset last_updated_at { get; set; } = DateTimeOffset.Now; // last time I updated this row!
+
+    public string last_updated_by { get; set; } = string.Empty;
+    public string created_by { get; set; } = string.Empty;
 }
